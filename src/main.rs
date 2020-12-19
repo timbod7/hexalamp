7ff331028c8d02caa90d4bfbcb7822d12d6c0f44
Tim Docker <tim@dockerz.net>
2020-12-19 15:37:51 +1100

    Imported rgb blinking example from https://github.com/smart-leds-rs/smart-leds-samples

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,79 @@
 #![no_main]
 #![no_std]
 
-// set the panic handler
-extern crate panic_semihosting;
+use panic_rtt_target as _;
+use rtt_target::rtt_init_default;
 
-use core::sync::atomic::{AtomicBool, Ordering};
-use cortex_m::peripheral::syst::SystClkSource;
-use cortex_m_rt::{entry, exception};
-use stm32f1xx_hal::prelude::*;
+use stm32f1xx_hal as hal;
+use ws2812_spi as ws2812;
 
-static TOGGLE_LED: AtomicBool = AtomicBool::new(false);
+use crate::hal::delay::Delay;
+use crate::hal::pac;
+use crate::hal::prelude::*;
+use crate::hal::spi::Spi;
+use crate::ws2812::Ws2812;
+use cortex_m::peripheral::Peripherals;
+
+use smart_leds::{SmartLedsWrite, RGB8};
+
+use cortex_m_rt::entry;
 
 #[entry]
 fn main() -> ! {
-    let mut core = cortex_m::Peripherals::take().unwrap();
-    let device = stm32f1xx_hal::stm32::Peripherals::take().unwrap();
-    let mut rcc = device.RCC.constrain();
-    let mut flash = device.FLASH.constrain();
+    rtt_init_default!();
 
-    let clocks = rcc
-        .cfgr
-        .use_hse(8.mhz())
-        .sysclk(16.mhz())
-        .freeze(&mut flash.acr);
+    if let (Some(dp), Some(cp)) = (pac::Peripherals::take(), Peripherals::take()) {
+        // Take ownership over the raw flash and rcc devices and convert them into the corresponding
+        // HAL structs
+        let mut flash = dp.FLASH.constrain();
+        let mut rcc = dp.RCC.constrain();
 
-    // configure the user led
-    let mut gpioc = device.GPIOC.split(&mut rcc.apb2);
-    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+        // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
+        // `clocks`
+        let clocks = rcc
+            .cfgr
+            .sysclk(48.mhz())
+            .pclk1(24.mhz())
+            .freeze(&mut flash.acr);
 
-    // configure SysTick to generate an exception every second
-    core.SYST.set_clock_source(SystClkSource::Core);
-    core.SYST.set_reload(clocks.sysclk().0);
-    core.SYST.enable_counter();
-    core.SYST.enable_interrupt();
+        // Acquire the GPIOA peripheral
+        let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
 
-    loop {
-        // sleep
-        cortex_m::asm::wfi();
-        if TOGGLE_LED.swap(false, Ordering::AcqRel) {
-            led.toggle().unwrap();
+        let pins = (
+            gpiob.pb13.into_alternate_push_pull(&mut gpiob.crh),
+            gpiob.pb14.into_floating_input(&mut gpiob.crh),
+            gpiob.pb15.into_alternate_push_pull(&mut gpiob.crh),
+        );
+        let mut delay = Delay::new(cp.SYST, clocks);
+
+        let spi = Spi::spi2(dp.SPI2, pins, ws2812::MODE, 3.mhz(), clocks, &mut rcc.apb1);
+
+        let mut data: [RGB8; 3] = [RGB8::default(); 3];
+        let empty: [RGB8; 3] = [RGB8::default(); 3];
+        let mut ws = Ws2812::new(spi);
+        loop {
+            data[0] = RGB8 {
+                r: 0,
+                g: 0,
+                b: 0x10,
+            };
+            data[1] = RGB8 {
+                r: 0,
+                g: 0x10,
+                b: 0,
+            };
+            data[2] = RGB8 {
+                r: 0x10,
+                g: 0,
+                b: 0,
+            };
+            ws.write(data.iter().cloned()).unwrap();
+            delay.delay_ms(1000 as u16);
+            ws.write(empty.iter().cloned()).unwrap();
+            delay.delay_ms(1000 as u16);
         }
     }
-}
-
-#[exception]
-fn SysTick() {
-    TOGGLE_LED.store(true, Ordering::Release);
+    loop {
+        continue;
+    }
 }
