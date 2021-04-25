f96c96be3de9c776e637b7041e17eca6632e7f86
Tim Docker <tim@dockerz.net>
2021-04-25 10:53:51 +1000

    Added layer 5

--- src/animation/anim2.rs
+++ src/animation/anim2.rs
@@ -1,7 +1,7 @@
 use smart_leds::{RGB8};
 use smart_leds::hsv::{Hsv, hsv2rgb};
 
-use super::{Animation, Frame, faddr};
+use super::{Animation, Frame, faddr, FRAME_XMAX, FRAME_YMAX};
 
 const BLACK: RGB8 = RGB8 {r: 0, g: 0, b: 0,};
 
@@ -26,16 +26,13 @@ impl Anim {
    fn next_frame(&mut self, frame: &mut Frame) -> u16 {
      self.framei = self.framei + 1;
 
-     for ci in 0..15 {
+     for ci in 0..FRAME_XMAX {
        let hue: u8 = (( (ci*8-self.framei) as u32 *255)/120) as u8;
        let c = hsv2rgb(Hsv{hue:hue, sat:255, val:255});
        let x : i16 = ci as i16;
-       frame[faddr(x, 0)] = c;
-       frame[faddr(x, 1)] = c;
-       frame[faddr(x, 2)] = c;
-       frame[faddr(x, 3)] = c;
-       frame[faddr(x, 4)] = c;
-       frame[faddr(x, 5)] = c;
+       for y in 0..FRAME_YMAX {
+        frame[faddr(x, y as i16 )] = c;
+       }
      }
      50
    }
