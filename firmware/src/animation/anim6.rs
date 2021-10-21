8810f52318f4fa62bfe6217fbf10d58d81f29b68
Tim Docker <tim@dockerz.net>
2021-10-22 10:21:04 +1100

    Factor colour calculation functions out of anim6

--- firmware/src/animation/anim6.rs
+++ firmware/src/animation/anim6.rs
@@ -1,133 +1,127 @@
+use super::{fill, Animation, CellAddr, Frame, XorShift32};
 use crate::animation::gamma::gamma;
-use smart_leds::{RGB8};
-use smart_leds::hsv::{Hsv, hsv2rgb};
-use super::{Animation, Frame, XorShift32, CellAddr, fill};
-
-
+use crate::animation::phase_color;
+use smart_leds::hsv::{hsv2rgb, Hsv};
+use smart_leds::RGB8;
 
 const NUM_PATTERNS: usize = 15;
 
 pub struct Anim {
-  rng: XorShift32,
-  colors: Colors,
-  framei: usize,
-  patterns: [Pattern; NUM_PATTERNS],
+    rng: XorShift32,
+    colors: Colors,
+    framei: usize,
+    patterns: [Pattern; NUM_PATTERNS],
 }
 
-
-impl Anim { 
-  pub fn new() -> Self {
-    let rng = XorShift32{a:456};
-    let bghue = 220u8;
-    let bgval = 64u8;
-    let fghue = 250u8;
-    let fgval = 255u8;
-    Anim {
-      rng,
-      colors: Colors {
-        bghue,
-        bgval,
-        fghue,
-        fgval,
-        bg: hsv2rgb(Hsv{hue:bghue, sat:255, val:gamma(bgval)})
-      },
-      framei: 0,
-      patterns:[
-        Pattern::new(CellAddr{x:0,y:0}, true),
-        Pattern::new(CellAddr{x:0,y:4}, true),
-        Pattern::new(CellAddr{x:0,y:8}, true),
-
-        Pattern::new(CellAddr{x:3,y:0}, true),
-        Pattern::new(CellAddr{x:3,y:4}, true),
-        Pattern::new(CellAddr{x:3,y:8}, true),
-
-        Pattern::new(CellAddr{x:6,y:0}, true),
-        Pattern::new(CellAddr{x:6,y:4}, true),
-        Pattern::new(CellAddr{x:6,y:8}, true),
-
-        Pattern::new(CellAddr{x:9,y:0}, true),
-        Pattern::new(CellAddr{x:9,y:4}, true),
-        Pattern::new(CellAddr{x:9,y:8}, true),
-
-        Pattern::new(CellAddr{x:12,y:0}, true),
-        Pattern::new(CellAddr{x:12,y:4}, true),
-        Pattern::new(CellAddr{x:12,y:8}, true),
-      ]
+impl Anim {
+    pub fn new() -> Self {
+        let rng = XorShift32 { a: 456 };
+        let bghue = 220u8;
+        let bgval = 64u8;
+        let fghue = 250u8;
+        let fgval = 255u8;
+        Anim {
+            rng,
+            colors: Colors {
+                bghue,
+                bgval,
+                fghue,
+                fgval,
+                bg: hsv2rgb(Hsv {
+                    hue: bghue,
+                    sat: 255,
+                    val: gamma(bgval),
+                }),
+            },
+            framei: 0,
+            patterns: [
+                Pattern::new(CellAddr { x: 0, y: 0 }, true),
+                Pattern::new(CellAddr { x: 0, y: 4 }, true),
+                Pattern::new(CellAddr { x: 0, y: 8 }, true),
+                Pattern::new(CellAddr { x: 3, y: 0 }, true),
+                Pattern::new(CellAddr { x: 3, y: 4 }, true),
+                Pattern::new(CellAddr { x: 3, y: 8 }, true),
+                Pattern::new(CellAddr { x: 6, y: 0 }, true),
+                Pattern::new(CellAddr { x: 6, y: 4 }, true),
+                Pattern::new(CellAddr { x: 6, y: 8 }, true),
+                Pattern::new(CellAddr { x: 9, y: 0 }, true),
+                Pattern::new(CellAddr { x: 9, y: 4 }, true),
+                Pattern::new(CellAddr { x: 9, y: 8 }, true),
+                Pattern::new(CellAddr { x: 12, y: 0 }, true),
+                Pattern::new(CellAddr { x: 12, y: 4 }, true),
+                Pattern::new(CellAddr { x: 12, y: 8 }, true),
+            ],
+        }
     }
-  }
- }
- 
- impl <I> Animation<I> for Anim {
+}
+
+impl<I> Animation<I> for Anim {
     fn next_frame(&mut self, _inputs: &I, frame: &mut Frame) -> u16 {
-      fill(frame, self.colors.bg);
-      for pi in 0..NUM_PATTERNS {
-        self.patterns[pi].next_frame(frame, self.framei, &mut self.rng, &self.colors);
-      }
-      self.framei += 1;
-      40
+        fill(frame, self.colors.bg);
+        for pi in 0..NUM_PATTERNS {
+            self.patterns[pi].next_frame(frame, self.framei, &mut self.rng, &self.colors);
+        }
+        self.framei += 1;
+        40
     }
- }
+}
 
 pub struct Colors {
-  bghue: u8,
-  bgval: u8,
-  fghue: u8,
-  fgval: u8,
-  bg: RGB8,
-}
-
-pub fn linterp(x: i32, x1: i32, x2: i32, y1: i32, y2: i32) -> i32 {
-  y1 + (y2 - y1) * (x - x1) / (x2 - x1)
+    bghue: u8,
+    bgval: u8,
+    fghue: u8,
+    fgval: u8,
+    bg: RGB8,
 }
 
 pub struct Pattern {
-  loc : CellAddr,
-  go_left: bool,
+    loc: CellAddr,
+    go_left: bool,
 }
 
 impl Pattern {
-  fn new(loc: CellAddr, go_left: bool) -> Pattern {
-    Pattern{loc,go_left}
-  }
-
-  fn next_frame(&mut self, frame: &mut Frame, framei: usize, _rng: &mut XorShift32, colors: &Colors) {
-    let hex0 = self.loc;
-    let hex1 = hex0.left().up();
-    let hex2 = hex1.up();
-    let hex3 = hex2.up();
-    let hex4 = hex3.right().down();
-    let hex5 = hex4.down();
-    
-    const PERIOD : usize = 200;    
-    const PERIOD2 : usize = PERIOD/2;
-
-
-    let phase = framei % PERIOD;
-
-    let (hue,val) = if  phase < PERIOD / 2  {
-      (
-        linterp( phase as i32, 0, PERIOD2 as i32, colors.bghue as i32, colors.fghue as i32),
-        linterp( phase as i32, 0, PERIOD2 as i32, colors.bgval as i32, colors.fgval as i32),
-      )
-    } else {
-      (
-        linterp( phase as i32, PERIOD2 as i32, PERIOD as i32, colors.fghue as i32, colors.bghue as i32),
-        linterp( phase as i32, PERIOD2 as i32, PERIOD as i32, colors.fgval as i32, colors.bgval as i32), 
-      )
-    };
-
-    let fg = hsv2rgb(Hsv{hue: hue as u8, sat:255, val:gamma(val as u8)});
-
-    if phase == PERIOD - 1 {
-      self.loc = if self.go_left {self.loc.left().left()} else  {self.loc.right().right()}
+    fn new(loc: CellAddr, go_left: bool) -> Pattern {
+        Pattern { loc, go_left }
     }
 
+    fn next_frame(
+        &mut self,
+        frame: &mut Frame,
+        framei: usize,
+        _rng: &mut XorShift32,
+        colors: &Colors,
+    ) {
+        const PERIOD: usize = 200;
 
-    frame[hex0.faddr()] = fg;
-    frame[hex1.faddr()] = fg;
-    frame[hex2.faddr()] = fg;
-    frame[hex3.faddr()] = fg;
-    frame[hex4.faddr()] = fg;
-    frame[hex5.faddr()] = fg;
-  }
+        let fg = phase_color(
+            PERIOD,
+            framei,
+            colors.bghue,
+            colors.bgval,
+            colors.fghue,
+            colors.fgval,
+        );
+
+        if framei % PERIOD == PERIOD - 1 {
+            self.loc = if self.go_left {
+                self.loc.left().left()
+            } else {
+                self.loc.right().right()
+            }
+        }
+
+        let hex0 = self.loc;
+        let hex1 = hex0.left().up();
+        let hex2 = hex1.up();
+        let hex3 = hex2.up();
+        let hex4 = hex3.right().down();
+        let hex5 = hex4.down();
+
+        frame[hex0.faddr()] = fg;
+        frame[hex1.faddr()] = fg;
+        frame[hex2.faddr()] = fg;
+        frame[hex3.faddr()] = fg;
+        frame[hex4.faddr()] = fg;
+        frame[hex5.faddr()] = fg;
+    }
 }
--- firmware/src/animation/mod.rs
+++ firmware/src/animation/mod.rs
@@ -1,26 +1,27 @@
+use smart_leds::hsv::{hsv2rgb, Hsv};
+use smart_leds::RGB8;
 
-use smart_leds::{RGB8};
+use crate::animation::gamma::gamma;
 
 pub const FRAME_SIZE: usize = 180;
 pub const FRAME_XMAX: usize = 15;
-pub const FRAME_YMAX: usize = 12; 
+pub const FRAME_YMAX: usize = 12;
 
 pub type Frame = [RGB8; FRAME_SIZE];
 
 // An animation with inputs of type I
 pub trait Animation<I> {
-  // Given any control inputs, generate the next frame of
-  // the animation, and return the delay in
-  // ms until the next;
-  fn next_frame(&mut self, input: &I, frame: &mut Frame) -> u16;
+    // Given any control inputs, generate the next frame of
+    // the animation, and return the delay in
+    // ms until the next;
+    fn next_frame(&mut self, input: &I, frame: &mut Frame) -> u16;
 }
 
 pub fn init_frame() -> Frame {
-  [RGB8 {r: 0, g: 0, b: 0,}; FRAME_SIZE]
+    [RGB8 { r: 0, g: 0, b: 0 }; FRAME_SIZE]
 }
 
-pub mod gamma;
-pub mod switcher;
+pub mod adcdisplay;
 pub mod anim0;
 pub mod anim1;
 pub mod anim2;
@@ -29,215 +30,277 @@ pub mod anim4;
 pub mod anim5;
 pub mod anim6;
 pub mod combo1;
-pub mod adcdisplay;
+pub mod gamma;
+pub mod switcher;
 
 use gamma::GAMMA;
 
- const ADDR: [[usize; FRAME_YMAX]; FRAME_XMAX] = [
-  [0, 1, 30, 31, 62, 63, 92, 93, 124, 125, 154, 155],
-  [2, 3, 32, 33, 64, 65, 94 ,95, 126, 127, 156, 157],
-  [4, 5, 34, 35, 66, 67, 96, 97, 128, 129, 158, 159],
-  [6, 7, 36, 37, 68, 69, 98, 99, 130, 131, 160, 161],
-  [8, 9, 38, 39, 70, 71, 100, 101, 132, 133, 162, 163],
-  [10, 11, 40, 41, 72, 73, 102, 103, 134, 135, 164, 165],
-  [12, 13, 42, 43, 74, 75, 104, 105, 136, 137, 166, 167],
-  [14, 15, 44, 45, 76, 77, 106, 107, 138, 139, 168, 169],
-  [16, 17, 46, 47, 78, 79, 108, 109, 140, 141, 170, 171],
-  [18, 19, 48, 49, 80, 81, 110, 111, 142, 143, 172, 173],
-  [20, 21, 50, 51, 82, 83, 112, 113, 144, 145, 174, 175],
-  [22, 23, 52, 53, 84, 85, 114, 115, 146, 147, 176, 177],
-  [24, 25, 54, 55, 86, 87, 116, 117, 148, 149, 178, 179],
-  [26, 27, 56, 57, 88, 89, 118, 119, 120, 121, 150, 151],
-  [28, 29, 58, 59, 60, 61, 90, 91, 122, 123, 152, 153],  
- ];
+const ADDR: [[usize; FRAME_YMAX]; FRAME_XMAX] = [
+    [0, 1, 30, 31, 62, 63, 92, 93, 124, 125, 154, 155],
+    [2, 3, 32, 33, 64, 65, 94, 95, 126, 127, 156, 157],
+    [4, 5, 34, 35, 66, 67, 96, 97, 128, 129, 158, 159],
+    [6, 7, 36, 37, 68, 69, 98, 99, 130, 131, 160, 161],
+    [8, 9, 38, 39, 70, 71, 100, 101, 132, 133, 162, 163],
+    [10, 11, 40, 41, 72, 73, 102, 103, 134, 135, 164, 165],
+    [12, 13, 42, 43, 74, 75, 104, 105, 136, 137, 166, 167],
+    [14, 15, 44, 45, 76, 77, 106, 107, 138, 139, 168, 169],
+    [16, 17, 46, 47, 78, 79, 108, 109, 140, 141, 170, 171],
+    [18, 19, 48, 49, 80, 81, 110, 111, 142, 143, 172, 173],
+    [20, 21, 50, 51, 82, 83, 112, 113, 144, 145, 174, 175],
+    [22, 23, 52, 53, 84, 85, 114, 115, 146, 147, 176, 177],
+    [24, 25, 54, 55, 86, 87, 116, 117, 148, 149, 178, 179],
+    [26, 27, 56, 57, 88, 89, 118, 119, 120, 121, 150, 151],
+    [28, 29, 58, 59, 60, 61, 90, 91, 122, 123, 152, 153],
+];
 
-
-fn faddr(x: i16, y:i16) -> usize {
-  ADDR[x.rem_euclid(FRAME_XMAX as i16) as usize][y.rem_euclid(FRAME_YMAX as i16) as usize]
+fn faddr(x: i16, y: i16) -> usize {
+    ADDR[x.rem_euclid(FRAME_XMAX as i16) as usize][y.rem_euclid(FRAME_YMAX as i16) as usize]
 }
-
 
 // fn faddr(x: i16, y:i16) -> usize {
 //   ADDR[(x as usize) % FRAME_XMAX][(y as usize) % FRAME_YMAX]
 // }
 
 fn fill(frame: &mut Frame, color: RGB8) {
-  for i in 0..FRAME_SIZE {
-    frame[i] = color;
-  }
+    for i in 0..FRAME_SIZE {
+        frame[i] = color;
+    }
 }
 
 // Fade the contents of the frame:
 //     u/v = 1  => leave as is
 //     u/v = 0  => fully black
-fn fade(frame: &mut Frame, u0: usize, v0:usize) -> () {
-  let u = GAMMA[u0 * (GAMMA.len()-1) / v0] as usize;
-  let v = 255;
-  for i in 0..FRAME_SIZE {
-    frame[i].r = ((frame[i].r as usize * u) / v) as u8;
-    frame[i].g = ((frame[i].g as usize * u) / v) as u8;
-    frame[i].b = ((frame[i].b as usize * u) / v) as u8;
-  }
-} 
-
-
+fn fade(frame: &mut Frame, u0: usize, v0: usize) -> () {
+    let u = GAMMA[u0 * (GAMMA.len() - 1) / v0] as usize;
+    let v = 255;
+    for i in 0..FRAME_SIZE {
+        frame[i].r = ((frame[i].r as usize * u) / v) as u8;
+        frame[i].g = ((frame[i].g as usize * u) / v) as u8;
+        frame[i].b = ((frame[i].b as usize * u) / v) as u8;
+    }
+}
 
 pub struct XorShift32 {
-  a: u32,
+    a: u32,
 }
 
 impl XorShift32 {
-  pub fn next(&mut self) -> u32 {
-      let mut x = self.a;
-      x ^= x << 13;
-      x ^= x << 17;
-      x ^= x >> 5;
-      self.a = x;
-      x
-  }
+    pub fn next(&mut self) -> u32 {
+        let mut x = self.a;
+        x ^= x << 13;
+        x ^= x << 17;
+        x ^= x >> 5;
+        self.a = x;
+        x
+    }
 }
 
+#[derive(Copy, Clone)]
+struct CellAddr {
+    x: usize,
+    y: usize,
+}
+
+impl CellAddr {
+    fn faddr(&self) -> usize {
+        ADDR[self.x][self.y]
+    }
+
+    fn left(&self) -> CellAddr {
+        CellAddr {
+            x: if self.x == 0 {
+                FRAME_XMAX - 1
+            } else {
+                self.x - 1
+            },
+            y: self.y,
+        }
+    }
+    fn right(&self) -> CellAddr {
+        CellAddr {
+            x: if self.x == FRAME_XMAX - 1 {
+                0
+            } else {
+                self.x + 1
+            },
+            y: self.y,
+        }
+    }
+    fn down(&self) -> CellAddr {
+        CellAddr {
+            x: self.x,
+            y: if self.y == 0 {
+                FRAME_YMAX - 1
+            } else {
+                self.y - 1
+            },
+        }
+    }
+    fn up(&self) -> CellAddr {
+        CellAddr {
+            x: self.x,
+            y: if self.y == FRAME_YMAX - 1 {
+                0
+            } else {
+                self.y + 1
+            },
+        }
+    }
+}
 
 #[derive(Copy, Clone)]
- struct CellAddr {
-   x : usize,
-   y : usize,
- }
-
- impl CellAddr {
-
-  fn faddr(&self) -> usize {
-    ADDR[self.x][self.y]
-  }
-
-  fn left(&self) -> CellAddr {
-    CellAddr{x: if self.x == 0 {FRAME_XMAX-1} else {self.x-1}, y:self.y}
-  }
-  fn right(&self) -> CellAddr {
-    CellAddr{x: if self.x == FRAME_XMAX-1 {0} else {self.x+1}, y:self.y}
-  }
-  fn down(&self) -> CellAddr {
-    CellAddr{x: self.x, y: if self.y == 0 {FRAME_YMAX-1} else {self.y-1}}
-  }
-  fn up(&self) -> CellAddr {
-    CellAddr{x: self.x, y: if self.y == FRAME_YMAX-1 {0} else {self.y+1}}
-  }
- }
-
- #[derive(Copy, Clone)]
- enum CellOrientation {
-   PointUp,
-   PointDown,
+enum CellOrientation {
+    PointUp,
+    PointDown,
 }
 
 impl CellOrientation {
-  fn from(cell: &CellAddr) -> CellOrientation {
-    if cell.y % 2 == 0 {
-     CellOrientation::PointUp 
-    } else {
-     CellOrientation::PointDown
+    fn from(cell: &CellAddr) -> CellOrientation {
+        if cell.y % 2 == 0 {
+            CellOrientation::PointUp
+        } else {
+            CellOrientation::PointDown
+        }
     }
-  }
 }
 
-
- #[derive(Copy, Clone)]
- enum CellType {
-   BEdge,
-   Internal,
-   TEdge
+#[derive(Copy, Clone)]
+enum CellType {
+    BEdge,
+    Internal,
+    TEdge,
 }
 
 impl CellType {
-
-  fn from(cell: &CellAddr) -> CellType {
-    if cell.y == 0 { 
-      CellType::BEdge
-    }  else if cell.y == FRAME_YMAX - 1 {
-      CellType::TEdge
-    } else {
-      CellType::Internal
-    }
-  }
-    
-  fn num_neighbours(&self) -> usize {
-    match self {
-      CellType::BEdge => 2,
-      CellType::Internal => 3,
-      CellType::TEdge => 2
-    }
-  }
-
-  fn neighbour(&self, cell: &CellAddr, i: usize) -> CellAddr {
-    match self {
-      CellType::BEdge => {
-        match i {
-          0 => cell.left().up(),
-          _ => cell.up()
-        }
-      }
-      CellType::Internal => {
-        if cell.y % 2 == 0 {
-          // up pointing
-          match i {
-            0 => cell.down(),
-            1 => cell.left().up(),
-            _ => cell.up()  
-          }
+    fn from(cell: &CellAddr) -> CellType {
+        if cell.y == 0 {
+            CellType::BEdge
+        } else if cell.y == FRAME_YMAX - 1 {
+            CellType::TEdge
         } else {
-          // down pointing
-          match i {
-            0 => cell.down(),
-            1 => cell.right().down(),
-            _ => cell.up()  
-          }
+            CellType::Internal
         }
-      },
-      CellType::TEdge => {
-        match i {
-          0 => cell.down(),
-          _ => cell.right().down()
-        }
-      }
     }
-  }
-}
 
+    fn num_neighbours(&self) -> usize {
+        match self {
+            CellType::BEdge => 2,
+            CellType::Internal => 3,
+            CellType::TEdge => 2,
+        }
+    }
+
+    fn neighbour(&self, cell: &CellAddr, i: usize) -> CellAddr {
+        match self {
+            CellType::BEdge => match i {
+                0 => cell.left().up(),
+                _ => cell.up(),
+            },
+            CellType::Internal => {
+                if cell.y % 2 == 0 {
+                    // up pointing
+                    match i {
+                        0 => cell.down(),
+                        1 => cell.left().up(),
+                        _ => cell.up(),
+                    }
+                } else {
+                    // down pointing
+                    match i {
+                        0 => cell.down(),
+                        1 => cell.right().down(),
+                        _ => cell.up(),
+                    }
+                }
+            }
+            CellType::TEdge => match i {
+                0 => cell.down(),
+                _ => cell.right().down(),
+            },
+        }
+    }
+}
 
 struct Trail<const TSIZE: usize> {
-  cells: [CellAddr; TSIZE],
-  headi: usize,
-  size: usize,
+    cells: [CellAddr; TSIZE],
+    headi: usize,
+    size: usize,
 }
 
-impl <const TSIZE: usize> Trail<TSIZE> {
- pub fn new() -> Self {
-   Trail {
-     cells: [CellAddr{x:0,y:0}; TSIZE],
-     headi:0,
-     size:0,
-   }
- }
+impl<const TSIZE: usize> Trail<TSIZE> {
+    pub fn new() -> Self {
+        Trail {
+            cells: [CellAddr { x: 0, y: 0 }; TSIZE],
+            headi: 0,
+            size: 0,
+        }
+    }
 
- pub fn head(&mut self) -> CellAddr {
-   self.cells[self.headi]
- }
+    pub fn head(&mut self) -> CellAddr {
+        self.cells[self.headi]
+    }
 
- pub fn push_head(&mut self, addr:CellAddr) {
-   self.headi = (self.headi + 1) % TSIZE;
-   self.cells[self.headi] = addr;
-   if self.size < TSIZE {
-     self.size += 1;
-   }
- }
+    pub fn push_head(&mut self, addr: CellAddr) {
+        self.headi = (self.headi + 1) % TSIZE;
+        self.cells[self.headi] = addr;
+        if self.size < TSIZE {
+            self.size += 1;
+        }
+    }
 
- pub fn pop_tail(&mut self) {
-   if self.size > 0 {
-     self.size -= 1;
-   }
- }
+    pub fn pop_tail(&mut self) {
+        if self.size > 0 {
+            self.size -= 1;
+        }
+    }
 
- pub fn cell(&self, i: usize) -> CellAddr {
-   self.cells[(self.headi + TSIZE - i) % TSIZE]
- }
+    pub fn cell(&self, i: usize) -> CellAddr {
+        self.cells[(self.headi + TSIZE - i) % TSIZE]
+    }
+}
+
+pub fn linterp(x: i32, x1: i32, x2: i32, y1: i32, y2: i32) -> i32 {
+    y1 + (y2 - y1) * (x - x1) / (x2 - x1)
+}
+
+pub fn phase_color(
+    period: usize,
+    framei: usize,
+    bghue: u8,
+    bgval: u8,
+    fghue: u8,
+    fgval: u8,
+) -> RGB8 {
+    let period2: usize = period / 2;
+
+    let phase = framei % period;
+
+    let (hue, val) = if phase < period / 2 {
+        (
+            linterp(phase as i32, 0, period2 as i32, bghue as i32, fghue as i32),
+            linterp(phase as i32, 0, period2 as i32, bgval as i32, fgval as i32),
+        )
+    } else {
+        (
+            linterp(
+                phase as i32,
+                period2 as i32,
+                period as i32,
+                fghue as i32,
+                bghue as i32,
+            ),
+            linterp(
+                phase as i32,
+                period2 as i32,
+                period as i32,
+                fgval as i32,
+                bgval as i32,
+            ),
+        )
+    };
+    hsv2rgb(Hsv {
+        hue: hue as u8,
+        sat: 255,
+        val: gamma(val as u8),
+    })
 }
